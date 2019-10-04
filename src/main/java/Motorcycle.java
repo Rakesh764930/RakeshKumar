3243b23f01f7fe5a7bc3904dc177a4729ad38290
RAKESH KUMAR <rakeshkumar92215@gmail.com>
2019-10-04 03:21:39 -0400

    variable : Look

--- src/main/java/Motorcycle.java
+++ src/main/java/Motorcycle.java
@@ -1,6 +1,11 @@
 public class Motorcycle extends Vehicle implements IPrintable {
     // Motorcycle's Additional variable
     String Brand;
+    enum Look{
+        Muscular,
+        Standard,
+        Racing
+    }
 
 
 
