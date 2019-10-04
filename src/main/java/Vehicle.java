93240f08b1dcccb5d4ace5a8560df79f228998e2
RAKESH KUMAR <rakeshkumar92215@gmail.com>
2019-10-04 03:53:52 -0400

    getters and setters

--- src/main/java/Vehicle.java
+++ src/main/java/Vehicle.java
@@ -3,11 +3,19 @@ public abstract class Vehicle implements IPrintable {
 private int Make;
   private   String Plate;
 
+  public int getMake() {
+    return Make;
+  }
 
+  public void setMake(int make) {
+    Make = make;
+  }
 
+  public String getPlate() {
+    return Plate;
+  }
 
-
-
-
-
+  public void setPlate(String plate) {
+    Plate = plate;
+  }
 }
