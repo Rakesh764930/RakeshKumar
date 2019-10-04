1555e91b59253e3998d139468cc15eb4dd13738e
RAKESH KUMAR <rakeshkumar92215@gmail.com>
2019-10-04 03:56:24 -0400

    update

--- src/main/java/Motorcycle.java
+++ src/main/java/Motorcycle.java
@@ -1,11 +1,7 @@
 public class Motorcycle extends Vehicle implements IPrintable {
     // Motorcycle's Additional variable
    private String Brand;
-    private enum Look{
-        Muscular,
-        Standard,
-        Racing
-    }
+    private double price;
 
 
 
