20ac73cc66223e45e63e9512835fd5eafb4bca8d
RAKESH KUMAR <rakeshkumar92215@gmail.com>
2019-10-04 03:57:15 -0400

    getters and setters

--- src/main/java/Motorcycle.java
+++ src/main/java/Motorcycle.java
@@ -3,11 +3,20 @@ public class Motorcycle extends Vehicle implements IPrintable {
    private String Brand;
     private double price;
 
+    public String getBrand() {
+        return Brand;
+    }
 
+    public void setBrand(String brand) {
+        Brand = brand;
+    }
 
+    public double getPrice() {
+        return price;
+    }
 
-
-
-
+    public void setPrice(double price) {
+        this.price = price;
+    }
 }
 
