94ba12a9fbdfde4d9242c108efa3ca1959798fcb
RAKESH KUMAR <rakeshkumar92215@gmail.com>
2019-10-04 03:11:10 -0400

    variable : TYPE

--- src/main/java/Car.java
+++ src/main/java/Car.java
@@ -1,6 +1,11 @@
 public class Car extends Vehicle implements IPrintable {
     // Car's Additional Variable
     String  Color;
+    enum type{
+        Sports,
+        Luxury,
+        Normal
+    }
 
 
 
