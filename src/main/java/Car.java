c4981b708950ae6c81314de3ee3e2e8373e3a689
RAKESH KUMAR <rakeshkumar92215@gmail.com>
2019-10-04 04:04:52 -0400

    new variable

--- src/main/java/Car.java
+++ src/main/java/Car.java
@@ -1,8 +1,25 @@
 public class Car extends Vehicle implements IPrintable {
     // Car's Additional Variable
     private String  Color;
+
+    public String getColor() {
+        return Color;
+    }
+
+    public void setColor(String color) {
+        Color = color;
+    }
+
+    public byte getNumberOfSeats() {
+        return NumberOfSeats;
+    }
+
+    public void setNumberOfSeats(byte numberOfSeats) {
+        NumberOfSeats = numberOfSeats;
+    }
+
     private byte NumberOfSeats;
-    
+
 
 
 
